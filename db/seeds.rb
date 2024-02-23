c24f92234e4aad52627f259d15ea6a9b66990b5c
Rebeca Yun <rebecajhyun@gmail.com>
2024-02-23 09:04:58 -0300

    Third challenge incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require
+
+
+10.times do
+  article = Article.new(
+    title: Faker::Movies::Avatar.character,
+    content: Faker::Movies::Avatar.quote
+  )
+  article.save!
+end
